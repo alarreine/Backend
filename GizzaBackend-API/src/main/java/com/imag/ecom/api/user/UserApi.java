8dfec3792c4aca0aa95514cdf9b3dc400b0f31fa
mickledur <michael.zodehougan@gmail.com>
2017-09-22 18:12:59 +0200

    Commandes final

--- GizzaBackend-API/src/main/java/com/imag/ecom/api/user/UserApi.java
+++ GizzaBackend-API/src/main/java/com/imag/ecom/api/user/UserApi.java
@@ -12,6 +12,7 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
@@ -21,6 +22,7 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import com.imag.ecom.user.UserRepository;
+import com.imag.ecom.commande.Commande;
 import com.imag.ecom.security.Secured;
 import com.imag.ecom.security.TokenServices;
 import com.imag.ecom.shared.Role;
@@ -63,6 +65,18 @@ public class UserApi {
 	public Response getAll() {
 		Map<String, List<User>> res = new HashMap<>();
 		res.put("data", repository.findAll());
+		return Response.ok(res, MediaType.APPLICATION_JSON_TYPE).build();
+	}
+
+	@GET
+	@Path("/get/commandes")
+	@Secured({ Role.ADMIN, Role.USER })
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getAllCommandes(@HeaderParam("Authorization") String token) {
+		String username = TokenServices.getUsername(token);
+		User u = repository.getById(username);
+		Map<String, List<Commande>> res = new HashMap<>();
+		res.put("data", u.getCommandes());
 		return Response.ok(res, MediaType.APPLICATION_JSON_TYPE).build();
 	}
 
